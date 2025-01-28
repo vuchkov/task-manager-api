0c81ac81a486da85a8a817cc9f0357cc197ecf9b
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2025-01-28 09:23:03 +0200

    Add JWT methods

--- config/auth.php
+++ config/auth.php
@@ -13,9 +13,13 @@ return [
     |
     */
 
-    'defaults' => [
+    /*'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+    ],*/
+    'defaults' => [
+        'guard' => 'api',
+        'passwords' => 'users',
     ],
 
     /*
@@ -35,9 +39,15 @@ return [
     |
     */
 
-    'guards' => [
+    /*'guards' => [
         'web' => [
             'driver' => 'session',
+            'provider' => 'users',
+        ],
+    ],*/
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
     ],
