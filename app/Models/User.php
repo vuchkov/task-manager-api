da16ac4987732d197b474d271fa18d3b4eaaeaf2
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2025-01-28 09:13:15 +0200

    Add JWT methods in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,11 +47,21 @@ class User extends Authenticatable implements JWTSubject
         ];
     }
 
-    public function getJWTIdentifier()
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
     {
         return $this->getKey();
     }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
     public function getJWTCustomClaims(): array
     {
         return [];
